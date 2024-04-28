f3261ecf610e46e952842037c5ea0d62d9221db3
VBortsov <mrxstepik@gmail.com>
2024-04-28 16:36:07 +0200

    evaluateData + triggerAlert methods

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -1,5 +1,7 @@
 package com.cardio_generator.generators;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import com.cardio_generator.outputs.OutputStrategy;
@@ -13,10 +15,17 @@ public class AlertGenerator implements PatientDataGenerator {
         AlertStates = new boolean[patientCount + 1];
     }
 
-    //public boolean evaluateData(){
-//
-    //}
-
+    public void evaluateData(PatientData patient, PatientDataCriteria criteria){
+        HashMap<String, Double> metrics = patient.getMetrics();
+        for(Map.Entry<String, Double> metric: metrics.entrySet()){
+            if(criteria.checkCriteria(metric.getKey(), metric.getValue()) == false) {
+                triggerAlert(new Alert(patient.getPatientID(), metric.getKey(), (int)System.currentTimeMillis()));
+            }
+        }
+    }
+    public void triggerAlert(Alert alert){
+        System.out.println("Patient: " + alert.getPatientID() + " has " + alert.getCondition() + ", need immediate help");
+    }
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {
@@ -42,4 +51,40 @@ public class AlertGenerator implements PatientDataGenerator {
             e.printStackTrace();
         }
     }
+}
+class PatientDataCriteria{
+
+
+    private HashMap<String, CriteriaBounds> criterias;
+    public PatientDataCriteria(){
+        criterias = new HashMap<String, CriteriaBounds>();
+    }
+    public PatientDataCriteria(HashMap<String, CriteriaBounds> criterias) {
+        this.criterias = criterias;
+    }
+    public void addCriteria(String featureName, Double leftBound, Double rightBound){
+        criterias.put(featureName, new CriteriaBounds(leftBound, rightBound));
+    }
+    public boolean checkCriteria(String featureName, Double val){
+        if(criterias.get(featureName).getLeftBound() > val || criterias.get(featureName).getRightBound() < val)
+            return false;
+        return true;
+    }
+}
+class CriteriaBounds{
+    public Double getLeftBound() {
+        return leftBound;
+    }
+    public Double getRightBound() {
+        return rightBound;
+    }
+
+    public CriteriaBounds(Double leftBound, Double rightBound) {
+        this.leftBound = leftBound;
+        this.rightBound = rightBound;
+    }
+
+    Double leftBound;
+    Double rightBound;
+
 }
