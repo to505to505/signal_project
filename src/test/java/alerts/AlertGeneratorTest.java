5fa06a4a75f48d1405df68647c3430d545b0be17
xstepik <[200~mrxst@xstepik MINGW64 ~$ git config --global user.name xstepikmrxst@xstepik MINGW64 ~$ git config --global user.namexstepikmrxst@xstepik MINGW64 ~$ git config --global user.email>
2024-06-09 16:51:08 +0200

    UML commit

---
+++ src/test/java/alerts/AlertGeneratorTest.java
@@ -0,0 +1,4 @@
+package alerts;
+
+public class AlertGeneratorTest {
+}
---
+++ src/test/java/data_management/PatientTest.java
@@ -0,0 +1,36 @@
+package data_management;
+
+import static org.junit.Assert.*;
+
+import com.data_management.Patient;
+import com.data_management.PatientRecord;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+
+public class PatientTest {
+
+    private Patient patient;
+
+    @Before
+    public void setUp() {
+        patient = new Patient(1);
+    }
+
+    @Test
+    public void testAddRecord() {
+        patient.addRecord(98.6, "Temperature", 1609459200000L);
+        List<PatientRecord> records = patient.getRecords(1609459200000L, 1609459200000L);
+        assertEquals(1, records.size());
+        assertEquals(98.6, records.get(0).getMeasurementValue(), 0.01);
+        assertEquals("Temperature", records.get(0).getRecordType());
+    }
+
+    @Test
+    public void testGetRecords() {
+        patient.addRecord(98.6, "Temperature", 1609459200000L);
+        patient.addRecord(99.1, "Temperature", 1609545600000L);
+        List<PatientRecord> records = patient.getRecords(1609459200000L, 1609545600000L);
+        assertEquals(2, records.size());
+    }
+}
